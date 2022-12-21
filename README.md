5da5575b85dd1ecd54b97a83ca741bfb9475e6fd
varghedj <dylanvarghese@gmail.com>
2022-12-21 16:32:55 -0500

    starting work on navbar

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ My portfolio website
 - If you encounter the following error (Parsing error: Cannot find module '@babel/preset-env'
 )
   https://github.com/varghedj/varghedj-portfolio/edit/main/README.md
+
+- CSS BEM styling convention
+  https://www.devbridge.com/articles/implementing-clean-css-bem-method/
--- frontend_react/src/components/navbar/navbar.jsx
+++ frontend_react/src/components/navbar/navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './navbar.scss'
+import {images} from '../../constants'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app-navbar-links'>
+        {['home', 'about','work', 'skills', 'contact'].map((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))
+        }
+      </ul>
+    </nav>
   )
 }
 
