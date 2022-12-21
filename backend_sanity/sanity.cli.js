cf99fb2670d35ac9eb2d75666c7cbc4fe49860a8
varghedj <dylanvarghese@gmail.com>
2022-12-21 14:37:25 -0500

    built sanity.io backend and created db schemas to be used later

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'lsw6y76e',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,32 @@
+
+export default {
+  name:'testimonials',
+  title:'Testimonials',
+  type: 'document',
+  fields:[
+      { 
+          name:'name',
+          title:'Name',
+          type: 'string'
+      },
+      {
+          name:'company',
+          title:'Company',
+          type:'string'
+      },
+      {
+          name:'imgurl',
+          title:'ImgUrl',
+          type: 'image',
+          options: {
+            // the hotspot function allows users to crop their uploaded images
+            hotspot: true,
+          },
+      },  
+      {
+          name:'feedback',
+          title:'Feedback',
+          type:'string'
+      }
+  ]
+}
